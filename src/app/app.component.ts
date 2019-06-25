ae53287f8c9d773d8d82eaa42fb3315fdd95d866
Juan Aguirre <judaguir@bancolombia.com.co>
2019-06-24 22:54:16 -0500

    My Angular PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './services/data.service';
+import { Joke } from './models/joke.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'AngularPWA';
+export class AppComponent implements OnInit {
+  title = `JDAC's App`;
+  joke: Joke;
+
+  constructor(
+    private updatesPWA: SwUpdate,
+    private dataService: DataService
+  ) {
+    updatesPWA.available.subscribe(() => {
+      updatesPWA.activateUpdate().then(() => document.location.reload());
+    });
+  }
+
+  ngOnInit(): void {
+    this.getJokes();
+  }
+
+  getJokes(): void {
+    this.dataService.getJokes().subscribe(
+      (res) => {
+        this.joke = res;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { DataService } from './services/data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [
+    DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
